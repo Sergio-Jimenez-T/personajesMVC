c0020d8d3ffb85dc871a4b55157d27cde7ca1e5b
Sergio Quetzal Jimenez Tinoco <sergio.q.jimenez.t@gmail.com>
2025-03-31 15:49:52 -0600

    Delete Personajes/app directory

--- Personajes/app/celulares.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `celulares` (
-  `id` int(11) NOT NULL,
-  `Marca` varchar(100) NOT NULL,
-  `Modelo` varchar(100) NOT NULL,
-  `Color` varchar(100) NOT NULL,
-  `Precio` varchar(100) NOT NULL,
-  `plan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
- 
