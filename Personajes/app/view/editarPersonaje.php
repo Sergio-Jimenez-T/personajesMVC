a60ecf56fdcbc609fbe82613e57ac10a193f9a73
Sergio Quetzal Jimenez Tinoco <sergio.q.jimenez.t@gmail.com>
2025-03-31 15:51:56 -0600

    Carpeta app actualizada
    Solucionamos error en editarPersonaje.php

---
+++ Personajes/app/view/editarPersonaje.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Editar</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Editar Personaje</h1>
+    </header>
+
+    <div class="container">
+        <div class="container2">
+
+		<form action="index.php?action=actualizarPersonaje" method="POST">
+			<input type="hidden" name="id" value="<?= $row->id ?>">
+			<label>Personaje:</label>
+			<input type="text" name="personaje" value="<?= $row->personaje ?>">
+			<label>Saga:</label>
+			<input type="text" name="saga" value="<?= $row->saga ?>">
+			<label>Especie:</label>
+			<input type="text" name="especie" value="<?= $row->especie ?>">
+			<button type="submit">Actualizar</button>
+		</form>
+
+        </div>
+
+    </div>
+</body>
+</html>
